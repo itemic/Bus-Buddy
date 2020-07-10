c1b8043dac7d9ea787dceb466330c7915f21f0e7
Terran Kroft <terrankroft@gmail.com>
2020-07-10 18:48:42 +0800

    Initial Commit

---
+++ Bus Buddy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bus Buddy
+//
+//  Created by Terran Kroft on 7/10/20.
+//  Copyright © 2020 itemic. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
